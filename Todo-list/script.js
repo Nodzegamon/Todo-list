59f49e04eaa29192004130a40894e49b7bb096f5
Nodzegamon <45968258+Nodzegamon@users.noreply.github.com>
2019-10-01 16:14:57 +0400

    Update script.js

--- Todo-list/script.js
+++ Todo-list/script.js
@@ -1,4 +1,4 @@
-Vue.component('task',{
+Vue.component('task',{//создаем vue компоненты
     props: ['data'],
     data() {
       return {
@@ -6,16 +6,16 @@ Vue.component('task',{
     },
     methods: {
       task_done(){
-        this.$emit('task_done')
+        this.$emit('task_done')//запускаетфункцию task_done через $emit
       }
     },
-    template: `
+    template: `// тут будет верстка самого компонента
     <div class="task">
       <div>
         <h3 class="task__title">{{data.title}}</h3>
         <p v-if="data.desc!=''" class="task__desc">{{data.desc}}</p>
       </div>
-      <button class="task__done" @click="task_done()">✔️</button>
+      <button class="task__done" @click="task_done()">✔️</button>//кнопочка для отметки выполнения задания
     </div>`
   })
   
@@ -29,7 +29,7 @@ Vue.component('task',{
       },
       tasks : [
         {
-          title: 'Изучить основы vue.js',
+          title: 'Какое-то очень важное задание №1',
           desc: 'Попробовать написать калькулятор',
         },
         {
@@ -38,15 +38,15 @@ Vue.component('task',{
         }
       ]
     },
-    methods: {
+    methods: { //описываем метод для добавления/удаления задач 
       add_task(){
-        if(this.new_task.title != ''){
-          this.tasks.push({
+        if(this.new_task.title != ''){ //если строка заполнена выполняется все что внутри скобок написано
+          this.tasks.push({ //добавляем пушем в конец массива
             title: this.new_task.title,
             desc: this.new_task.desc
           });
-          this.new_task.title='';
-          this.new_task.desc='';
+          this.new_task.title=''; //очищяем поля ввода
+          this.new_task.desc=''; //очищяем поля ввода
         }
       },
       delete_task(id){
